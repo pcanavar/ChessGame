64b894cebf3b8c8f894c26539f4a28f86b35fe9c
Philippe Canavarro <pgcanavarro@gmail.com>
2021-07-08 21:06:30 +0100

    Add project files.

---
+++ Xadres/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Tabuleiro;
+
+namespace Xadres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P = new Posicao(3, 4);
+
+
+
+
+
+        }
+    }
+}
